3a9b12a6128ef2a60fb1cb88f3908170d927f303
blerer <b_lerer@hotmail.com>
2014-06-24 11:09:45 +0200

    Fix UnitTest name

--- src/test/java/io/horizondb/model/core/records/TimeSeriesRecordTest.java
+++ src/test/java/io/horizondb/model/core/records/TimeSeriesRecordTest.java
@@ -29,7 +29,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class TimeseriesRecordTest {
+public class TimeSeriesRecordTest {
 
     /**
      * The record type used during the tests.
